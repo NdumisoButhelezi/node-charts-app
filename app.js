0e0c2adbf7fce9b94faeb57c186ac8a1817695d8
PLanet-09AI <ndumisobuthelezi028@gmail.com>
2024-10-18 01:02:21 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// app.js
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files (for charts and HTML)
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
